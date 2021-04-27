f5dcc614d3280612b3743f2b2e44ef79bf985e1e
GillesRitchmond <gritchmond@gmail.com>
2021-04-27 15:15:45 -0400

    first commit

---
+++ View/footer.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+        rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+        crossorigin="anonymous">
+
+        <!-- Bootstrap JS -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+        
+        <!-- GOOGLE ICONS -->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Icons">
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="Style/style.css" type="text/css">
+    </head>
+
+    <body>
+        <nav class="navbar navbar-light shadow-lg bg-white menu-footer">
+            <div class="container list">
+                <ul class="me-auto mb-2 mb-lg-0">
+                    <li><a class="navbar-brand" href="#">
+                        <span class="material-icons">home</span>
+                    </a></li>
+                    
+                    <li><a class="navbar-brand" href="#">
+                        <span class="material-icons md-3">list</span>
+                    </a></li>
+                    
+                    <li><a class="navbar-brand" href="#">
+                        <span class="material-icons">shopping_cart</span>
+                    </a></li>
+                    
+                    <li><a class="navbar-brand" href="#">
+                        <span class="material-icons">account_circle</span>
+                    </a></li>
+                    
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
+
+
+<!-- <a class="navbar-brand" href="#">
+    <span class="material-icons">home</span>
+</a>
+<a class="navbar-brand" href="#">
+    <span class="material-icons md-3">list</span>
+</a>
+<a class="navbar-brand" href="#">
+    <span class="material-icons">shopping_cart</span>
+</a>
+<a class="navbar-brand" href="#">
+    <span class="material-icons">account_circle</span>
+</a> -->
